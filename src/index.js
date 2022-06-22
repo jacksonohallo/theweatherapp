406b2535b45c72d9b282c99c0d3931fede93f0c1
jacksonohallo <jacksonohallo@gmail.com>
2022-06-22 06:07:28 +0300

    add html element into js file using query selector

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const app = document.querySelector(".weather-app");
+const temp = document.querySelector(".temp");
+const dateOutput = document.querySelector(".date");
+const timeOutput = document.querySelector(".time");
+const conditionOutput = document.querySelector(".condition");
+const nameOutput = document.querySelector(".name");
+const icone = document.querySelector(".icon");
+const cloudOutput = document.querySelector(".cloud");
+const humidityOutput = document.querySelector(".humidity");
+
